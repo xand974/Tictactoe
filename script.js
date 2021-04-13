68eadcc5e1b93c974d17079dfe886f2380dee09a
Alexandre Malet <xand974@gmail.com>
2021-04-13 11:53:42 +0200

    finalisation du design tic tac toe - debut du script

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const btn = document.querySelectorAll('button');
+const myTitle = document.querySelector('h1');
 
+//Anim Boutton
 btn.forEach(element => {
     element.addEventListener('mouseover', () => {
         element.classList.toggle('animBtn');
@@ -10,3 +12,19 @@ btn.forEach(element => {
 
     });
 })
+
+
+//Anim Titre
+myTitle.addEventListener('mouseenter', () => {
+    myTitle.classList.add('animH1');
+})
+
+myTitle.addEventListener('mouseleave', () => {
+    myTitle.classList.remove('animH1');
+})
+
+
+
+//Jeu Tic Tac Toe
+
+const player = document.querySelectorAll('p');
